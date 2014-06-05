72a05d65217da2ff55a71c9db0678866538cbc80
kanechu <kanechu@itdept.com.hk>
2014-06-05 18:28:09 +0800

    add cell

---
+++ itcrm/Cell_armacct_browse.m
@@ -0,0 +1,29 @@
+//
+//  Cell_armacct_browse.m
+//  itcrm
+//
+//  Created by itdept on 14-6-5.
+//  Copyright (c) 2014年 itdept. All rights reserved.
+//
+
+#import "Cell_armacct_browse.h"
+
+@implementation Cell_armacct_browse
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
